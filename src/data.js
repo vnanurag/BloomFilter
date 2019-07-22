54a72fb1aab36364a87d91b64bf1a8d62f1937b5
Anurag Vutukuri <37963348+vnanurag@users.noreply.github.com>
2019-07-22 14:33:50 -0500

    Adding comments

--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
+// Initial test data ti initialize the memory
+// A local file from the computer can also be uploaded for larger data
 class Names{
     words = () => {
         return ['Anu', 'Anurag', 'Alan', 'Alankrutha', 'Chinch', 'Chava', 'Thejasri', 'Kkr', 'Dj', 'Tangi', 'Paddu'];
