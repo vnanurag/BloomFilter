ccecd281fb1b70f4e0b4a5b69a0b401413517c60
Anurag Vutukuri <37963348+vnanurag@users.noreply.github.com>
2019-07-22 00:21:58 -0500

    Adding build files

---
+++ build/precache-manifest.5a7f7fc21d2a82cb0901cd82a9f2a13a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5da069b12be76be39f3dcb4903875f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "849c453788e0a417e214",
+    "url": "/static/css/main.48c089cd.chunk.css"
+  },
+  {
+    "revision": "db0c0faed99554c7cb65",
+    "url": "/static/js/2.fde99a95.chunk.js"
+  },
+  {
+    "revision": "849c453788e0a417e214",
+    "url": "/static/js/main.8496dc73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
